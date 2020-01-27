d2007d07617b9576e5a92d2d924d845905ba54a0
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-27 11:53:07 +0530

    Added colours to Arrows

--- exp3/mol.js
+++ exp3/mol.js
@@ -7,26 +7,27 @@ $(function() {
   viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:7.5,y:7.5,z:7.5},radius:.5,color:'black',radiusRadio:1.0,
   mid:1.0,   clickable:true,
   callback:function(){
-      this.color.setHex(0xFF0000FF);
+      this.color.setHex(0xFFFF3352);
       viewer.render( );
   }});
   viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:-7.5,y:-7.5,z:7.5},radius:.5,color:'black',radiusRadio:1.0,
   mid:1.0,   clickable:true,
   callback:function(){
-      this.color.setHex(0xFF0000FF);
+      this.color.setHex(0xFFFF3352);
       viewer.render( );
   }});
   viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:7.5,y:-7.5,z:-7.5},radius:.5,color:'black',radiusRadio:1.0,
   mid:1.0,   clickable:true,
   callback:function(){
-      this.color.setHex(0xFF0000FF);
+      this.color.setHex(0xFFFF3352);
       viewer.render( );
   }});
   viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:-7.5,y:7.5,z:-7.5},radius:.5,color:'black',radiusRadio:1.0,
   mid:1.0,   clickable:true,
   callback:function(){
-      this.color.setHex(0xFF0000FF);
+      this.color.setHex(0xFFFF3352);
       viewer.render( );
+      
   }});
   viewer.addSphere({ center: {x:0, y:0, z:0}, radius: 2.0, color: 'skyblue' });
   viewer.addSphere({ center: {x:6, y:-6, z:-6}, radius: 2.0, color: 'white' });
