d02ce60a21e269e13775e2fab058dfe92f83472d
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-27 12:43:43 +0530

    Ethane Basic Structure

--- exp3/ethane.js
+++ exp3/ethane.js
@@ -23,16 +23,8 @@ $(function() {
         this.color.setHex(0xFFFF3352);
         viewer.render( );
     }});
-    viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:-7.5,y:7.5,z:-7.5},radius:.5,color:'black',radiusRadio:1.0,
-    mid:1.0,   clickable:true,
-    callback:function(){
-        this.color.setHex(0xFFFF3352);
-        viewer.render( );
-        
-    }});
     viewer.addSphere({ center: {x:0, y:0, z:0}, radius: 2.0, color: 'skyblue' });
     viewer.addSphere({ center: {x:6, y:-6, z:-6}, radius: 2.0, color: 'white' });
-    viewer.addSphere({ center: {x:-6, y:6, z:-6}, radius: 2.0, color: 'white' });
     viewer.addSphere({ center: {x:-6, y:-6, z:6}, radius: 2.0, color: 'white' });
     viewer.addSphere({ center: {x:15, y:0, z:0}, radius: 2.0, color: 'skyblue' });
     viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:18,y:0,z:0},radius:.5,color:'black',radiusRadio:1.0,
@@ -41,6 +33,27 @@ $(function() {
         this.color.setHex(0xFFFF3352);
         viewer.render( );
         
+    }});
+    viewer.addSphere({ center: {x:21, y:-6, z:-6}, radius: 2.0, color: 'white' });
+    viewer.addSphere({ center: {x:9, y:6, z:-6}, radius: 2.0, color: 'white' });
+    viewer.addSphere({ center: {x:9, y:-6, z:6}, radius: 2.0, color: 'white' });
+    viewer.addArrow({ start: {x:15 , y:0, z:0}, end: {x:7.5,y:7.5,z:-7.5},radius:.5,color:'black',radiusRadio:1.0,
+    mid:1.0,   clickable:true,
+    callback:function(){
+        this.color.setHex(0xFFFF3352);
+        viewer.render( );
+    }});
+    viewer.addArrow({ start: {x:15 , y:0, z:0}, end: {x:7.5,y:-7.5,z:7.5},radius:.5,color:'black',radiusRadio:1.0,
+    mid:1.0,   clickable:true,
+    callback:function(){
+        this.color.setHex(0xFFFF3352);
+        viewer.render( );
+    }});
+    viewer.addArrow({ start: {x:15 , y:0, z:0}, end: {x:22.5,y:-7.5,z:-7.5},radius:.5,color:'black',radiusRadio:1.0,
+    mid:1.0,   clickable:true,
+    callback:function(){
+        this.color.setHex(0xFFFF3352);
+        viewer.render( );
     }});
     viewer.zoomTo();
     viewer.render();
