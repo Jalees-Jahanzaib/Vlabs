520bf4be80fb60f12c3c9fd76657334a42f3c1bf
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-27 11:47:49 +0530

    1

--- exp3/mol.js
+++ exp3/mol.js
@@ -4,10 +4,30 @@ $(function() {
   let viewer = $3Dmol.createViewer( element, config );
   console.log(viewer);
   viewer.addSphere({ center: {x:6, y:6, z:6}, radius: 2.0, color: 'white' });
-  viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:7.5,y:7.5,z:7.5},radius:.5,color:'black'});
-  viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:-7.5,y:-7.5,z:7.5},radius:.5,color:'black'});
-  viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:7.5,y:-7.5,z:-7.5},radius:.5,color:'black'});
-  viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:-7.5,y:7.5,z:-7.5},radius:.5,color:'black'});
+  viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:7.5,y:7.5,z:7.5},radius:.5,color:'black',radiusRadio:1.0,
+  mid:1.0,   clickable:true,
+  callback:function(){
+      this.color.setHex(0xFF0000FF);
+      viewer.render( );
+  }});
+  viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:-7.5,y:-7.5,z:7.5},radius:.5,color:'black',radiusRadio:1.0,
+  mid:1.0,   clickable:true,
+  callback:function(){
+      this.color.setHex(0xFF0000FF);
+      viewer.render( );
+  }});
+  viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:7.5,y:-7.5,z:-7.5},radius:.5,color:'black',radiusRadio:1.0,
+  mid:1.0,   clickable:true,
+  callback:function(){
+      this.color.setHex(0xFF0000FF);
+      viewer.render( );
+  }});
+  viewer.addArrow({ start: {x:0 , y:0, z:0}, end: {x:-7.5,y:7.5,z:-7.5},radius:.5,color:'black',radiusRadio:1.0,
+  mid:1.0,   clickable:true,
+  callback:function(){
+      this.color.setHex(0xFF0000FF);
+      viewer.render( );
+  }});
   viewer.addSphere({ center: {x:0, y:0, z:0}, radius: 2.0, color: 'skyblue' });
   viewer.addSphere({ center: {x:6, y:-6, z:-6}, radius: 2.0, color: 'white' });
   viewer.addSphere({ center: {x:-6, y:6, z:-6}, radius: 2.0, color: 'white' });
